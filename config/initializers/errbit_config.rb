da92ba5a3c7c67b685b9e2d947676c8e2639d976
Sergey Ovsiannikov <sergey.ovsiannikov@hnd.sm.ua>
2015-03-04 10:30:13 +0300

    Remove handle dev errors option from errbit

--- config/initializers/errbit_config.rb
+++ config/initializers/errbit_config.rb
@@ -3,6 +3,5 @@ Airbrake.configure do |config|
   config.host    = 'errbit.hnd.sm.ua'
   config.port    = 80
   config.secure  = config.port == 443
-  config.development_environments = []
   config.ignore_only  = []
 end
