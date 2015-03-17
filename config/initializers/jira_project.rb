a44f59e4afa5f464aea55ba0456564b1dc26ab5f
Dmitriy Baklikov <dmitriy@railsmuffin.com>
2015-03-17 12:03:19 +0300

    [ID-101] Refactor jira_project.rb

--- config/initializers/jira_project.rb
+++ config/initializers/jira_project.rb
@@ -1,19 +1,15 @@
 JIRA::Resource::Project.class_eval do
   # Returns all the issues for this project
   def issues(start_index=0)
-    response = client.get(client.options[:rest_base_path] + "/search?jql=project%3D'#{key}'&startIndex=#{start_index}")
-    json = self.class.parse_json(response.body)
+    json = get_to("/search?jql=project%3D'#{key}'&startIndex=#{start_index}")
     json['issues'].map do |issue|
       client.Issue.build(issue)
     end
   end
 
   def issues_by_filter(filter_id, start_index=0)
-    response = client.get(client.options[:rest_base_path] + "/filter/#{filter_id}?startIndex=#{start_index}")
-    filter_data = self.class.parse_json(response.body)
-
-    response = client.get(filter_data['searchUrl'])
-    json = self.class.parse_json(response.body)
+    filter_data = get_to("/filter/#{filter_id}?startIndex=#{start_index}")
+    json = get_to(filter_data['searchUrl'], true)
 
     json['issues'].map do |issue|
       client.Issue.build(issue)
@@ -21,18 +17,19 @@ JIRA::Resource::Project.class_eval do
   end
 
   def issue_with_name_expand
-    response = client.get(client.options[:rest_base_path] + "/search?jql=project%3D'#{key}'+AND+issuetype+%3D+%22New+Feature%22&maxResults=1&expand=names")
-    json = self.class.parse_json(response.body)
-    client.Issue.build(json)
+    client.Issue.build(get_to("/search?jql=project%3D'#{key}'+AND+issuetype+%3D+%22New+Feature%22&maxResults=1&expand=names"))
   end
 
   def asignable_users
-    response = client.get(client.options[:rest_base_path] + "/user/assignable/search?project=#{self.key}")
-    json = self.class.parse_json(response.body)
+    get_to("/user/assignable/search?project=#{self.key}")
   end
 
   def user_permissions
-    response = client.get(client.options[:rest_base_path] + "/mypermissions?projectKey=#{self.key}")
-    json = self.class.parse_json(response.body)
+    get_to("/mypermissions?projectKey=#{self.key}")
+  end
+
+  def get_to(path, full_path=false)
+    get_path = full_path ? path : client.options[:rest_base_path] + path
+    self.class.parse_json(client.get(get_path).body)
   end
 end
