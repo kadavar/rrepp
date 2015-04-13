fd245a77af9a1702683e57eb2201ade9dd286388
Dmitriy Baklikov <dmitriy@railsmuffin.com>
2015-04-13 21:02:42 +0300

    [ID-125] Add stop project logic

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,27 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, only: [:start, :stop, :force_sync]
+
   def index
     ProjectsHandler.perform
 
     @projects = Project.all
+  end
+
+  def start
+  end
+
+  def stop
+    Process.kill('SIGTERM', @project.pid)
+
+    redirect_to projects_path
+  end
+
+  def force_sync
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
 
-  resources :projects, only: [:index, :new, :create, :destroy]
+  resources :projects, only: [:index, :new, :create, :destroy] do
+    member do
+      get :stop
+      get :start
+      get :force_sync
+    end
+  end
+
   resources :project_logs, only: [:index, :show]
   resources :project_configs
 
