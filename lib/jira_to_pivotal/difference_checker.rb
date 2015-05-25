60b841adb4f571b2b113522963a38e26f6dcecc3
Sergey Ovsiannikov <sergey.o@railsmuffin.com>
2015-05-25 17:41:44 +0300

    [JTP-67] refactor difference checker class

--- lib/jira_to_pivotal/difference_checker.rb
+++ lib/jira_to_pivotal/difference_checker.rb
@@ -1,5 +1,5 @@
 class JiraToPivotal::DifferenceChecker < JiraToPivotal::Base
-  attr_reader :story_attrs, :jira_issue, :j2p_issue,  :config
+  attr_reader :config, :user_permissions
 
   def initialize(project, config)
     @user_permissions = JiraToPivotal::Jira::UserPermissions.new(project)
@@ -7,86 +7,81 @@ class JiraToPivotal::DifferenceChecker < JiraToPivotal::Base
   end
 
   def main_attrs_difference?(story_attrs, j2p_issue)
-    @story_attrs = story_attrs
-    @jira_issue  = j2p_issue.issue
-    @j2p_issue   = j2p_issue
-
-    story_issue_diff?
+    difference_between_pivotal_jira?(jira_issue: j2p_issue.issue, story_attrs: story_attrs, j2p_issue: j2p_issue)
   end
 
   def status_difference?(jira_issue, story)
-    status_diff(jira_issue, story)
+    status?(jira_issue, story)
   end
 
   private
 
-  def story_issue_diff?
-    summary_diff || description_diff || task_type_diff || reporter_diff || asignee_diff || estimates_diff
+  def difference_between_pivotal_jira?(params)
+    summary?(params) || description?(params) || task_type?(params) ||
+    reporter?(params) || asignee?(params) || estimates?(params)
   end
 
-  def story_reporter
-    @story_attrs['fields']['reporter']
+  def summary?(params)
+    params[:story_attrs]['fields']['summary'] != params[:jira_issue].fields['summary']
   end
 
-  def story_assignee
-    @story_attrs['fields']['assignee']
+  def description?(params)
+    params[:story_attrs]['fields']['description'] != params[:jira_issue].fields['description'].to_s
   end
 
-  def jira_reporter
-    jira_issue.fields['reporter']
+  def task_type?(params)
+    return false if params[:j2p_issue].subtask?
+    params[:story_attrs]['fields']['issuetype']['id'] != params[:jira_issue].fields['issuetype']['id']
   end
 
-  def jira_assignee
-    jira_issue.fields['assignee']
+  def estimates?(params)
+    return false if params[:j2p_issue].bug? || params[:j2p_issue].chore? || params[:j2p_issue].subtask?
+
+    points_field_id = config[:custom_fields].key(config['jira_custom_fields']['pivotal_points'])
+    params[:story_attrs]['fields'][points_field_id] != params[:jira_issue].fields[points_field_id] || empty_estimate?(params)
   end
 
-  def summary_diff
-    story_attrs['fields']['summary'] != jira_issue.fields['summary']
+  def empty_estimate?(params)
+    params[:jira_issue].fields['timeoriginalestimate'].nil?
   end
 
-  def description_diff
-    story_attrs['fields']['description'] != jira_issue.fields['description'].to_s
-  end
+  def reporter?(params)
+    return false unless user_permissions.modify_reporter?
 
-  def task_type_diff
-    return false if j2p_issue.subtask?
-    story_attrs['fields']['issuetype']['id'] != jira_issue.fields['issuetype']['id']
-  end
-
-  def estimates_diff
-    return false if j2p_issue.bug? || j2p_issue.chore? || j2p_issue.subtask?
-    story_attrs['fields'][points_field_id] != jira_issue.fields[points_field_id] || empty_estimate?
-  end
-
-  def empty_estimate?
-    jira_issue.fields['timeoriginalestimate'].nil?
-  end
-
-  def reporter_diff
-    return false unless @user_permissions.modify_reporter?
-
-    if story_reporter.present? && jira_reporter.present?
-      story_reporter['name'] != jira_reporter['name']
+    if story_reporter(params) && jira_reporter(params)
+      story_reporter(params)['name'] != jira_reporter(params)['name']
     else
-      story_reporter.present? && !jira_reporter.present?
+      story_reporter(params) && !jira_reporter(params)
     end
   end
 
-  def asignee_diff
-    return false unless @user_permissions.assign_issue?
+  def asignee?(params)
+    return false unless user_permissions.assign_issue?
 
-    if story_assignee.present? && story_assignee.present?
-      story_assignee['name'] != story_assignee['name']
+    if story_assignee(params) && jira_assignee(params)
+      story_assignee(params)['name'] != jira_assignee(params)['name']
     else
-      story_assignee.present? && !story_assignee.present?
+      story_assignee(params) && !jira_assignee(params)
     end
   end
 
-  def status_diff(jira_issue, story)
+  def status?(jira_issue, story)
     story.current_story_status_to_issue_status != jira_issue.fields['status']['name']
   end
 
-  def points_field_id
-    points_custom_field ||= config[:custom_fields].key(config['jira_custom_fields']['pivotal_points'])
+  def story_reporter(params)
+    params[:story_attrs]['fields']['reporter']
+  end
+
+  def story_assignee(params)
+    params[:story_attrs]['fields']['assignee']
+  end
+
+  def jira_reporter(params)
+    params[:jira_issue].fields['reporter']
+  end
+
+  def jira_assignee(params)
+    params[:jira_issue].fields['assignee']
   end
 end
