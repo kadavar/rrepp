58db3a64c6b5e01cce0a4975a5bdca359c1bf620
Sergey Ovsiannikov <sergey.o@railsmuffin.com>
2015-06-10 13:01:35 +0300

    [ID-120] delete file before delete model

--- app/models/project/config.rb
+++ app/models/project/config.rb
@@ -8,4 +8,15 @@ class Config < ActiveRecord::Base
 
   accepts_nested_attributes_for :jira_issue_types
   accepts_nested_attributes_for :jira_custom_fields
+
+  after_update :update_config_file
+  before_destroy :delete_config_file
+
+  def delete_config_file
+    File.delete(Rails.root.join "project_configs/#{name}.yml")
+  end
+
+  def update_config_file
+    # TODO: Write logic
+  end
 end
