36c63248dbd0ec1c181da2f87cb13463013c9ee2
Alexander Starodubtcev <astarodubtcev@yandex.ru>
2015-08-13 17:22:01 +0300

    modal test

--- spec/features/projects_force_sync_spec.rb
+++ spec/features/projects_force_sync_spec.rb
@@ -3,14 +3,23 @@ require 'rails_helper'
 describe 'force sync', :js do
   let!(:project) { create :project, :online, :with_config }
 
-  before do
-    visit projects_path
-  end
+  before { visit projects_path }
 
-  before do
-    click_link("id-#{project.id}", match: :first)
-  end
-  specify 'starts sync worker' do
-    expect(config).to eq {}
+  describe 'opens modal' do
+    before { click_link("id-#{project.id}", match: :first) }
+
+    specify 'modal exists' do
+      expect(page).to have_css('input[name="jira_password"]')
+      expect(page).to have_css('input[name="pivotal_token"]')
+    end
+
+    describe 'modal closing on cancel' do
+      before { click 'Close' }
+
+      specify 'modal closed' do
+        expect(page).not_to have_css('input[name="jira_password"]')
+        expect(page).not_to have_css('input[name="pivotal_token"]')
+      end
+    end
   end
 end
