1b0704d22cca4dc41a3d7f992c3eaf8fa91622e8
ALexandr <alex.lukin@jetruby.com>
2015-10-29 18:33:17 +0300

    config_composer_spec was added

--- spec/services/config_composer_spec.rb
+++ spec/services/config_composer_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
-describe  ConfigComposer do
+describe ConfigComposer do
 
   describe '#compose_project_config' do
-    let!(:project) { create :project, :online, :with_config }
+    let!(:project) { create :project, :online }
+    let(:config) { create :config, :issues_and_custom_fields }
+    let!(:issue_type) { config.jira_issue_types.first }
+    let!(:custom_field) { config.jira_custom_fields.first }
     subject(:compose) { ConfigComposer.new.compose_project_config(project) }
-
     context 'when return the right config' do
-      let (:new_fields) { { "project_id" =>project.id,
-                            "project_name" => project.name,
-                            "jira_custom_fields" => {} ,
-                            "jira_issue_types" => {} } }
+      before { project.config = config }
+      let (:new_fields) { {"project_id" => project.id,
+                           "project_name" => project.name,
+                           "jira_custom_fields" =>
+                            { "#{custom_field.name}" => "#{custom_field.value}" },
+                           "jira_issue_types" =>
+                            { "#{issue_type.name}" => issue_type.jira_id } } }
       let(:attrs) { project.config.attributes }
 
       it { is_expected.to include(new_fields) }
-      it { is_expected.to include(attrs)  }
+      it { is_expected.to include(attrs) }
     end
   end
 
