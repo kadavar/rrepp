826e8c9a9b3f5773c4d753928c99db1f27432954
AlexandrLukin <alex.lukin@jetruby.com>
2015-11-02 21:33:11 +0700

    add JiraToPivotal::Config spec

---
+++ spec/j2p/config_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe JiraToPivotal::Config do
+
+  let(:config) { { somekey:'somekey', custom_fields: 'custom fields', ownership_handler: 'ownership_handler' } }
+  subject (:j2p_conf) { JiraToPivotal::Config.new(config) }
+  describe '#[]' do
+    context 'when key is not exist ' do
+      let(:key) { 'not_exist' }
+      it { expect(j2p_conf.[](key)).to eq nil }
+    end
+    context 'when key is exist' do
+      let(:key) { :somekey }
+      it { expect(j2p_conf.[](key)).to eq 'somekey' }
+    end
+  end
+
+  describe '#airbrake_message_parameters' do
+    context 'when return except hash' do
+      let(:result) { { somekey: 'somekey' } }
+      it { expect(j2p_conf.airbrake_message_parameters).to eql result }
+    end
+  end
+end
